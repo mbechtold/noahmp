66a05676f51e94b4fb73db8fc54aaec4f727f25c
mbechtold <michel.bechtold@kuleuven.be>
2026-03-25 13:19:04 +0100

    coldstart

--- src/SoilWaterMainMod.F90
+++ src/SoilWaterMainMod.F90
@@ -219,11 +219,11 @@ contains
 
        ! Use stored WTD (continuous across timesteps)
 
-       !z_wt_begin = -WaterTableDepth
-       !WTD_begin  = WaterTableDepth
-       z_wt_begin = FindWaterTable(W_soil_peat, thetas_peat, ae_peat, &
-       bb_peat, z_col_bot_peat, -WaterTableDepth)
-       WTD_begin  = -z_wt_begin
+       z_wt_begin = -WaterTableDepth
+       WTD_begin  = WaterTableDepth
+       !z_wt_begin = FindWaterTable(W_soil_peat, thetas_peat, ae_peat, &
+       !bb_peat, z_col_bot_peat, -WaterTableDepth)
+       !WTD_begin  = -z_wt_begin
     endif
 
     ! Peatland: Ivanov runoff (uses diagnosed WTD)
@@ -367,11 +367,11 @@ contains
           enddo
 
          ! Debug: Write out SoilLiqWaterOrig and z_wt_begin
-         write(*,*) 'DEBUG: z_wt_begin = ', z_wt_begin
-         write(*,*) 'DEBUG: SoilLiqWaterOrig:'
-         do LoopInd1 = 1, NumSoilLayer
-            write(*,*) '  Layer', LoopInd1, SoilLiqWaterOrig(LoopInd1)
-         enddo
+         !write(*,*) 'DEBUG: z_wt_begin = ', z_wt_begin
+         !write(*,*) 'DEBUG: SoilLiqWaterOrig:'
+         !do LoopInd1 = 1, NumSoilLayer
+         !   write(*,*) '  Layer', LoopInd1, SoilLiqWaterOrig(LoopInd1)
+         !enddo
 
           ! Redistribute water that exceeds porosity to neighbors
           do LoopInd1 = 1, NumSoilLayer
@@ -470,13 +470,13 @@ contains
                           - (1.0_kind_noahmp - f_soil) * RunoffSubsurface * SoilTimeStep
 
          ! Debug: Write out all fluxes used in FSW_change_flux
-         write(*,*) 'DEBUG: FSW_change_flux terms:'
-         write(*,*) '  InfilRateSfc_FSW_change = ', InfilRateSfc_FSW_change
-         write(*,*) '  SoilTimeStep = ', SoilTimeStep
-         write(*,*) '  f_soil = ', f_soil
-         write(*,*) '  EvapGroundNet = ', EvapGroundNet
-         write(*,*) '  Transpiration = ', Transpiration
-         write(*,*) '  RunoffSubsurface = ', RunoffSubsurface
+         !write(*,*) 'DEBUG: FSW_change_flux terms:'
+         !write(*,*) '  InfilRateSfc_FSW_change = ', InfilRateSfc_FSW_change
+         !write(*,*) '  SoilTimeStep = ', SoilTimeStep
+         !write(*,*) '  f_soil = ', f_soil
+         !write(*,*) '  EvapGroundNet = ', EvapGroundNet
+         !write(*,*) '  Transpiration = ', Transpiration
+         !write(*,*) '  RunoffSubsurface = ', RunoffSubsurface
 
           ! --- Compute total soil water after backward transfer [m] ---
           W_soil_peat = 0.0_kind_noahmp
@@ -490,10 +490,10 @@ contains
           ! --- Diagnose final WTD from total water (same as equilibrium path) ---
           W_fsw_begin = SurfaceWaterStorage(z_wt_begin)
           W_total_peat = W_soil_peat + W_fsw_begin + FSW_change_flux / 1000.0_kind_noahmp
-          !z_wt_end = FindWaterTableTotal(W_total_peat, thetas_peat, ae_peat, &
-          !    bb_peat, z_col_bot_peat, z_wt_begin)
-          z_wt_end = FindWaterTable(W_soil_peat, thetas_peat, ae_peat, &
+          z_wt_end = FindWaterTableTotal(W_total_peat, thetas_peat, ae_peat, &
               bb_peat, z_col_bot_peat, z_wt_begin)
+          !z_wt_end = FindWaterTable(W_soil_peat, thetas_peat, ae_peat, &
+          !    bb_peat, z_col_bot_peat, z_wt_begin)
           WaterTableDepth = -z_wt_end
 
           ! Equilibrium soil water at the new WTD [m]
@@ -517,11 +517,11 @@ contains
           enddo
 
          ! Debug: Write out SoilLiqWater after backward transfer and z_wt_end
-         write(*,*) 'DEBUG: z_wt_end = ', z_wt_end
-         write(*,*) 'DEBUG: SoilLiqWater after backward transfer:'
-         do LoopInd1 = 1, NumSoilLayer
-            write(*,*) '  Layer', LoopInd1, SoilLiqWater(LoopInd1)
-         enddo
+         !write(*,*) 'DEBUG: z_wt_end = ', z_wt_end
+         !write(*,*) 'DEBUG: SoilLiqWater after backward transfer:'
+         !do LoopInd1 = 1, NumSoilLayer
+         !   write(*,*) '  Layer', LoopInd1, SoilLiqWater(LoopInd1)
+         !enddo
 
           ! Pass 2: additive normalization
           !mean_delta_peat = (W_soil_check - W_soil_eq_end) / z_col_bot_peat
