7a8195188f2e22b58de30fa1f3a57f56ec8a6982
mbechtold <michel.bechtold@kuleuven.be>
2026-04-04 21:15:37 +0200

    coldstart fix 2

--- src/SoilWaterMainMod.F90
+++ src/SoilWaterMainMod.F90
@@ -166,7 +166,8 @@ contains
               f_soil                 => noahmp%water%state%f_soil                   ,& ! inout, fraction of flux in and out of soil [-]
               SoilLiqWaterMin        => noahmp%water%state%SoilLiqWaterMin         ,& ! out,   minimum soil liquid water content [m3/m3]
               DepthSoilLayer         => noahmp%config%domain%DepthSoilLayer         ,& ! in,    depth [m] of layer-bottom from soil surface
-              TranspWatLossSoilMean  => noahmp%water%flux%TranspWatLossSoilMean      & ! inout, mean transpiration water loss from soil layers [m/s]
+              TranspWatLossSoilMean  => noahmp%water%flux%TranspWatLossSoilMean     ,& ! inout, mean transpiration water loss from soil layers [m/s]
+              WaterStorageTotBeg     => noahmp%water%state%WaterStorageTotBeg         & ! inout, total water storage at beginning [mm]
              )
 ! ----------------------------------------------------------------------
 
@@ -247,6 +248,43 @@ contains
        !z_wt_begin = FindWaterTable(W_soil_peat, thetas_peat, ae_peat, &
        !bb_peat, z_col_bot_peat, -WaterTableDepth)
        !WTD_begin  = -z_wt_begin
+
+       ! --- Start-of-timestep WTD cap: correct initial state if WTD > z_col_bot ---
+       if ( WTD_begin > WTD_max_peat ) then
+          W_soil_before_cap = W_soil_peat
+          WTD_original      = WTD_begin
+          WaterTableDepth   = WTD_max_peat
+          z_wt_begin        = -WaterTableDepth
+          WTD_begin         = WaterTableDepth
+
+          ! Reset soil moisture to equilibrium at capped WTD
+          do LoopInd1 = 1, NumSoilLayer
+             if (LoopInd1 == 1) then
+                d_top_peat = 0.0_kind_noahmp
+             else
+                d_top_peat = abs(DepthSoilLayer(LoopInd1 - 1))
+             endif
+             d_bot_peat = abs(DepthSoilLayer(LoopInd1))
+             SoilLiqWater(LoopInd1) = EquilibriumSMMicroTopo(d_top_peat, d_bot_peat, &
+                 WaterTableDepth, thetas_peat, ae_peat, bb_peat)
+             SoilMoisture(LoopInd1) = SoilLiqWater(LoopInd1) + SoilIce(LoopInd1)
+          enddo
+
+          ! Recompute W_soil_peat from corrected profile
+          W_soil_peat = 0.0_kind_noahmp
+          do LoopInd1 = 1, NumSoilLayer
+             W_soil_peat = W_soil_peat + &
+                 SoilLiqWater(LoopInd1) * abs(ThicknessSnowSoilLayer(LoopInd1))
+          enddo
+          W_soil_after_cap = W_soil_peat
+
+          ! Adjust WaterStorageTotBeg so balance check starts from corrected state
+          WaterStorageTotBeg = WaterStorageTotBeg + &
+              (W_soil_after_cap - W_soil_before_cap) * 1000.0_kind_noahmp
+
+          write(*,*) 'WARNING: WTD capped at start, z_col_bot=', WTD_max_peat, &
+                     ' original WTD=', WTD_original
+       endif
     endif
 
     ! Peatland: Ivanov runoff (uses diagnosed WTD)
@@ -831,7 +869,7 @@ contains
 
           ! Route excess water to subsurface runoff [mm/s] for mass conservation
           RunoffSubsurface = RunoffSubsurface + &
-              (W_soil_before_cap - W_soil_after_cap) / SoilTimeStep
+              (W_soil_before_cap - W_soil_after_cap) * 1000.0_kind_noahmp / SoilTimeStep
 
           ! Update FloodedFraction
           FloodedFraction = FloodedFrac(z_wt_end)
